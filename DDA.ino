74eac30ca5bd3c18fc079e81e2ba18afa2f1c039
kanaya281264139 <35449023+kanaya281264139@users.noreply.github.com>
2019-08-28 22:11:35 +0900

    Add files via upload

--- DDA.ino
+++ DDA.ino
@@ -1,9 +1,9 @@
 #include <TDK_Diamond_Seeds.h>
 
-uint16_t dValue;           // dynamic sensor value
-uint16_t sValue;         // static sensor value
-uint16_t alarmThreshold = 20;  // adjust to change alarm sensitivty
-bool triggered = false;        // becomes true when diary removed
+float dValue;                 // dynamic sensor value
+float sValue;                 // static sensor value
+float alarmThreshold = 0.5;    // adjust to change alarm sensitivty
+bool triggered = false;       // becomes true when diary removed
 int p;
 ///////////////////////////////////////////////////////////////////////////////
 void setup()
@@ -11,9 +11,34 @@ void setup()
   // Initialize integral library.
   TDSs.begin();
 
-  for (p=0; p<12; p=p+1) {
-      TDSs.setPixelColor(p,0,0,200);
-  }
+  p=0;
+  TDSs.setPixelColor(p,0,0,200);
+  p=1;
+  TDSs.setPixelColor(p,0,0,200);
+  p=2;
+  TDSs.setPixelColor(p,0,0,200);
+  p=3;
+  TDSs.setPixelColor(p,0,0,200);
+  p=4;
+  TDSs.setPixelColor(p,0,0,200);
+  p=5;
+  TDSs.setPixelColor(p,0,0,200);
+  p=6;
+  TDSs.setPixelColor(p,0,0,200);
+  p=7;
+  TDSs.setPixelColor(p,0,0,200);
+  p=8;
+  TDSs.setPixelColor(p,0,0,200);
+  p=9;
+  TDSs.setPixelColor(p,0,0,200);
+  p=10;
+  TDSs.setPixelColor(p,0,0,200);
+  p=11;
+  TDSs.setPixelColor(p,0,0,200);
+
+//for (p=0; p<12; p=p+1) {
+//  TDSs.setPixelColor(p,0,0,200);
+//}
 
   // Wait for button press.
   while ( (TDSs.leftButton()  == false) && 
@@ -25,14 +50,35 @@ void setup()
       TDSs.setPixelColor(p,0,0,0);
       delay(100);      
   }
-  
+
   // Compute sensor value as average of 5 readings.
   sValue = 0;
   for (p=0; p<5; p=p+1) {
-    dValue = TDSs.Tesla();
+    dValue = TDSs.AccelX();
     sValue = sValue + dValue;
   }
   sValue = sValue / 5;
+  
+/*  sValue = 0; 
+
+  p=0;
+  dValue = TDSs.Tesla();
+  sValue = sValue + dValue;
+  p=1;
+  dValue = TDSs.Tesla();
+  sValue = sValue + dValue;
+  p=2;
+  dValue = TDSs.Tesla();
+  sValue = sValue + dValue;
+  p=3;
+  dValue = TDSs.Tesla();
+  sValue = sValue + dValue;
+  p=4;
+  dValue = TDSs.Tesla();
+  sValue = sValue + dValue;
+
+  sValue = sValue / 5;
+*/
 
   delay(1000);
 }
@@ -42,7 +88,7 @@ void loop()
 {
 
     // Get current sensor value.
-    dValue = TDSs.Tesla();
+    dValue = TDSs.AccelX();
     
     Serial.println(dValue);
     
